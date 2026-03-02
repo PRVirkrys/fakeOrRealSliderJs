213346ce7bd0ee80b1725fd58dcccd694b641fb0
PRVirkrys <pala.3633@gmail.com>
2026-03-02 13:56:54 -0500

    primer commit

---
+++ script.js
@@ -0,0 +1,234 @@
+//Capturar elementos
+
+//Textos
+
+//Modals
+const gameOverModalElement = document.getElementById("gameOverModal");
+
+//Variables por defecto
+let lives = 3;
+let score = 0;
+let success = 10;
+let index = 0;
+
+//imagenes
+let photos = [
+  // REALES
+  {
+    src: "imgs/real-1.jpg",
+    title: "Patio urbano en silencio",
+    isReal: true,
+  },
+  {
+    src: "imgs/real-2.jpg",
+    title: "Luz cálida en la oscuridad",
+    isReal: true,
+  },
+  {
+    src: "imgs/real-3.jpg",
+    title: "Colores de primavera",
+    isReal: true,
+  },
+  {
+    src: "imgs/real-4.jpg",
+    title: "Orilla al amanecer",
+    isReal: true,
+  },
+  {
+    src: "imgs/real-5.jpg",
+    title: "Bosque entre la niebla",
+    isReal: true,
+  },
+
+  // IA
+  {
+    src: "imgs/ia-1.png",
+    title: "Esperando el tren",
+    isReal: false,
+  },
+  {
+    src: "imgs/ia-2.png",
+    title: "Lectura en calma",
+    isReal: false,
+  },
+  {
+    src: "imgs/ia-3.png",
+    title: "Reflejos bajo la lluvia",
+    isReal: false,
+  },
+  {
+    src: "imgs/ia-4.png",
+    title: "Preparando la cena",
+    isReal: false,
+  },
+  {
+    src: "imgs/ia-5.png",
+    title: "Montañas en espejo",
+    isReal: false,
+  },
+];
+
+//Cambio de imagenes - slider funcionamiento
+function updateScene() {
+  // Captura de HTML
+  const sceneImageElement = document.getElementById("sceneImage");
+  const sceneTitleElement = document.getElementById("sceneTitle");
+
+  sceneImageElement.src = photos[index].src;
+  sceneImageElement.alt = photos[index].title;
+
+  sceneTitleElement.textContent = photos[index].title;
+}
+
+// imageCounter
+function imgCounter() {
+  const sceneCounterElement = document.getElementById("sceneCounter");
+  let imgTotal = photos.length;
+  let imgActual = index + 1;
+  sceneCounterElement.textContent = `${imgActual} / ${imgTotal}`;
+}
+
+function indexValidation() {
+  if (index >= photos.length) {
+    index = 0;
+  } else if (index < 0) {
+    index = 0;
+  }
+}
+
+function imgPrev() {
+  index--;
+  indexValidation();
+  updateScene();
+  imgCounter();
+}
+
+function imgNext() {
+  index++;
+  indexValidation();
+  updateScene();
+  imgCounter();
+}
+
+function sortImages() {
+  //Alterar orden de imagenes
+  photos.sort(() => Math.random() - 0.5);
+}
+
+/**** Cambiar imagen *****/
+function changeImage() {
+  // Captura de botones slidder
+  const btnPrevElement = document.getElementById("btnPrev");
+  const btnNextElement = document.getElementById("btnNext");
+
+  // Llamada al evento click funcion
+
+  btnPrevElement.addEventListener("click", function () {
+    imgPrev();
+    resetFeebackImage();
+  });
+  btnNextElement.addEventListener("click", function () {
+    imgNext();
+    resetFeebackImage();
+  });
+
+  // llamada al evento boton funcion
+  document.addEventListener("keydown", (event) => {
+    if (event.key === "ArrowRight") {
+      imgNext();
+      resetFeebackImage();
+    }
+    if (event.key === "ArrowLeft") {
+      imgPrev();
+      resetFeebackImage();
+    }
+  });
+}
+
+//** Validacion de imagen correcta o falsa***//
+function userRes() {
+  //captura de botones
+  const btnCanonElement = document.getElementById("btnCanon");
+  const btnFakeElement = document.getElementById("btnFake");
+
+  btnCanonElement.addEventListener("click", () => {
+    let userRes = true;
+    imageValidation(userRes);
+  });
+
+  btnFakeElement.addEventListener("click", () => {
+    let userRes = false;
+    imageValidation(userRes);
+  });
+}
+
+function resetFeebackImage() {
+  let feedbackTextElement = document.getElementById("feedbackText");
+  feedbackTextElement.textContent = "Elige una opción 👀";
+}
+
+function imageValidation(userRes) {
+  let feedbackTextElement = document.getElementById("feedbackText");
+  let photoIsReal = photos[index].isReal;
+  if (photoIsReal === userRes) {
+    console.log("Si es real");
+    score += success;
+    scoreCounter();
+    feedbackTextElement.textContent = "¡Correcto! Es una imagen real 📷";
+  } else if (photoIsReal === userRes) {
+    console.log("Si la imagen es fake");
+    scoreCounter();
+    feedbackTextElement.textContent = "¡Correcto! Está generada por IA 🤖";
+  } else if (photoIsReal == true && userRes == false) {
+    feedbackTextElement.textContent = "Incorrecto. Es una imagen real 📷";
+    lives--;
+    livesHearts(lives);
+  } else if (photoIsReal == false && userRes == true) {
+    feedbackTextElement.textContent = "Incorrecto. Está generada por IA 🤖";
+    lives--;
+    livesHearts(lives);
+  } else {
+    feedbackTextElement.textContent = "Elige una opción para comenzar 👀";
+  }
+}
+
+// Mostrar corazones
+function livesHearts(lives) {
+  const livesElement = document.getElementById("lives");
+  const hearth = "❤️ ";
+  if (lives > 0) {
+    livesElement.textContent = "";
+    const hearthCounts = hearth.repeat(lives);
+    livesElement.textContent = hearthCounts;
+  } else {
+    //Aqui cuando vidas llegan a 0
+    livesElement.textContent = " ";
+    //Abrir modal para reiniciar todo
+  }
+}
+
+// restar Game
+function restartGame() {
+  lives = 3;
+  score = 0;
+  imgCounter();
+  sortImages();
+  updateScene();
+  livesHearts(lives);
+}
+
+//Score
+function scoreCounter() {
+  const scoreElement = document.getElementById("score");
+  scoreElement.textContent = score;
+}
+
+//EJECUCION DE FUNCIONES
+document.addEventListener("DOMContentLoaded", function () {
+  restartGame();
+  changeImage();
+  userRes();
+  scoreCounter();
+  const btnRestart = document.getElementById("btnRestart");
+  btnRestart.addEventListener("click", restartGame);
+});
